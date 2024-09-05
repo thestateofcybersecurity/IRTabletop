2caecb8886af4fc0d77ff2395da8081719927a03
thestateofcybersecurity <98850458+thestateofcybersecurity@users.noreply.github.com>
2024-09-05 14:58:16 -0600

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,7 @@
+import '../styles/globals.css'  // Adjust the path if your global.css is located elsewhere
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
