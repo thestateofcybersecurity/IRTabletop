e90e75842faf5c6813bb8ac0ff356ce02839f734
thestateofcybersecurity <98850458+thestateofcybersecurity@users.noreply.github.com>
2024-09-05 13:47:37 -0600

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,1 +1,9 @@
-
+export default function Footer() {
+  return (
+    <footer className="bg-gray-200 p-4 mt-8">
+      <div className="container mx-auto text-center">
+        <p>&copy; 2024 IR Tabletop Generator. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+}
