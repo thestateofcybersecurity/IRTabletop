24f814fd90fb7a873955b0634e40d883ac086e13
thestateofcybersecurity <98850458+thestateofcybersecurity@users.noreply.github.com>
2024-09-05 14:57:47 -0600

    Create register.js

---
+++ pages/register.js
@@ -0,0 +1,10 @@
+import RegistrationForm from '../components/RegistrationForm';
+
+export default function Register() {
+  return (
+    <div className="container mx-auto px-4">
+      <h1 className="text-3xl font-bold text-center my-8">Create an Account</h1>
+      <RegistrationForm />
+    </div>
+  );
+}
