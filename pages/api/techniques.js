3ffd7f9e12c17bb832a7ff6dda71f07519a77e1f
thestateofcybersecurity <98850458+thestateofcybersecurity@users.noreply.github.com>
2024-09-05 15:31:42 -0600

    Update techniques.js

--- pages/api/techniques.js
+++ pages/api/techniques.js
@@ -2,9 +2,8 @@ import { connectToDatabase } from '../../lib/mongodb';
 
 export default async function handler(req, res) {
   try {
-    const client = await clientPromise;
-    const db = client.db('mitre_cache');
-    const tactics = await db.collection('techniques').find({}).toArray();
+    const { db } = await connectToDatabase();
+    const techniques = await db.collection('techniques').find({}).toArray();
     res.status(200).json(techniques);
   } catch (error) {
     console.error('Error fetching techniques:', error);
