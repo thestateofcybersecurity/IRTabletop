af4250449e9411e60d031cc22b9d8fd1fbcfa397
thestateofcybersecurity <98850458+thestateofcybersecurity@users.noreply.github.com>
2024-09-05 15:00:18 -0600

    Update RegistrationForm.js

--- components/RegistrationForm.js
+++ components/RegistrationForm.js
@@ -1,4 +1,4 @@
-import { connectToDatabase } from '../../../lib/mongodb';
+import { connectToDatabase } from '../../lib/mongodb';
 import bcrypt from 'bcryptjs';
 
 export default async function handler(req, res) {
