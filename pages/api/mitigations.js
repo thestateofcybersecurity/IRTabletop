b74e9fb04116ba767d0c8a993cca63281f8f38b9
thestateofcybersecurity <98850458+thestateofcybersecurity@users.noreply.github.com>
2024-09-05 14:16:23 -0600

    Create mitigations.js

---
+++ pages/api/mitigations.js
@@ -0,0 +1,13 @@
+import clientPromise from '../../lib/mongodb';
+
+export default async function handler(req, res) {
+  try {
+    const client = await clientPromise;
+    const db = client.db('mitre_cache');
+    const tactics = await db.collection('mitigations').find({}).toArray();
+    res.status(200).json(mitigations);
+  } catch (error) {
+    console.error('Error fetching mitigations:', error);
+    res.status(500).json({ error: 'Failed to fetch mitigations' });
+  }
+}
