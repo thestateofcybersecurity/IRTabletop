380c86ba0156b09f1153565774a8e638f52ed154
thestateofcybersecurity <98850458+thestateofcybersecurity@users.noreply.github.com>
2024-09-05 15:10:36 -0600

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,6 +1,5 @@
-import { ObjectId } from 'mongodb';
-import bcrypt from 'bcryptjs';
 import { connectToDatabase } from '../lib/mongodb';
+import bcrypt from 'bcryptjs';
 
 export async function createUser(userData) {
   const { db } = await connectToDatabase();
@@ -25,13 +24,4 @@ export async function createUser(userData) {
 export async function findUserByEmail(email) {
   const { db } = await connectToDatabase();
   return db.collection('users').findOne({ email });
-}
-
-export async function findUserById(id) {
-  const { db } = await connectToDatabase();
-  return db.collection('users').findOne({ _id: ObjectId(id) });
-}
-
-export async function validatePassword(user, inputPassword) {
-  return bcrypt.compare(inputPassword, user.password);
 }
