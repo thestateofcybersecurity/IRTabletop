34ee4111a9bc1411a6bcf2aa9d41943bee5c1bdb
thestateofcybersecurity <98850458+thestateofcybersecurity@users.noreply.github.com>
2024-09-05 15:36:37 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -56,10 +56,6 @@ export default function Home() {
 
   return (
     <div className="container mx-auto px-4">
-      <Head>
-        <title>IR Tabletop Generator</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
 
       <Header user={user} onLogout={handleLogout} />
     
