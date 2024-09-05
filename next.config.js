61089430be5265ae6cf553f5753a595e8794a8fa
thestateofcybersecurity <98850458+thestateofcybersecurity@users.noreply.github.com>
2024-09-05 13:48:05 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,8 @@
-
+module.exports = {
+  reactStrictMode: true,
+  env: {
+    MONGODB_URI: process.env.MONGODB_URI,
+    MONGODB_DB: process.env.MONGODB_DB,
+    JWT_SECRET: process.env.JWT_SECRET,
+  },
+};
