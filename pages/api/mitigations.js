d91d2ca3e89e398025902a4c27ad2757d0a43084
thestateofcybersecurity <98850458+thestateofcybersecurity@users.noreply.github.com>
2024-09-05 15:31:53 -0600

    Update mitigations.js

--- pages/api/mitigations.js
+++ pages/api/mitigations.js
@@ -2,9 +2,8 @@ import { connectToDatabase } from '../../lib/mongodb';
 
 export default async function handler(req, res) {
   try {
-    const client = await clientPromise;
-    const db = client.db('mitre_cache');
-    const tactics = await db.collection('mitigations').find({}).toArray();
+    const { db } = await connectToDatabase();
+    const mitigations = await db.collection('mitigations').find({}).toArray();
     res.status(200).json(mitigations);
   } catch (error) {
     console.error('Error fetching mitigations:', error);
