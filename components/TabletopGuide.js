c4d13db4f5c4846b8e86119a6ba03dd215a0dbcf
thestateofcybersecurity <98850458+thestateofcybersecurity@users.noreply.github.com>
2024-09-06 08:33:38 -0600

    Update TabletopGuide.js

--- components/TabletopGuide.js
+++ components/TabletopGuide.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { getRandomInject } from './Injects';
 
 export default function TabletopGuide({ scenario, addAction }) {
