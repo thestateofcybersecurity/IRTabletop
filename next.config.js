b51e2bc764955bc248ad33b991b864ae087dcc90
thestateofcybersecurity <98850458+thestateofcybersecurity@users.noreply.github.com>
2024-09-11 10:55:38 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,5 +4,9 @@ module.exports = {
     MONGODB_URI: process.env.MONGODB_URI,
     MONGODB_DB: process.env.MONGODB_DB,
     JWT_SECRET: process.env.JWT_SECRET,
+    AUTH0_SECRET: process.env.AUTH0_SECRET,
+    AUTH0_BASE_URL: process.env.AUTH0_BASE_URL,
+    AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+    AUTH0_ISSUER_BASE_URL: process.env.AUTH0_ISSUER_BASE_URL,
   },
 };
