08e1de066d933e25d9238c60fdb8702facb68a4a
thestateofcybersecurity <98850458+thestateofcybersecurity@users.noreply.github.com>
2024-09-27 21:19:33 -0600

    Update login.js

--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -46,7 +46,7 @@ export default async function handler(req, res) {
     res.status(200).json({ 
       message: 'Login successful', 
       user: { id: user._id, username: user.username, email: user.email },
-      token 
+      token,
     });
   } catch (error) {
     console.error('Login error:', error);
